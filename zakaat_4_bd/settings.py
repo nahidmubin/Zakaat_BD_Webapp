44461ced74d82272d99a571dbb99b1e5edb41a7c
nahidmubin <nahidmubin@yahoo.com>
2024-07-04 23:08:09 +0600

    Made debug False in settings.py

--- zakaat_4_bd/settings.py
+++ zakaat_4_bd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h7$3rj9ycyjbse=qws#y9bcar7@#l*h=en&gy*k@8bp92fi_z@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
